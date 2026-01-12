cc4384880755de72bc2bc074c6c91602b1e5390a
Rombt <rombtnet@gmail.com>
2026-01-12 12:24:19 +0200

    a configuration and a structure of simple  test

---
+++ tests/structure_simple_test.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('название теста', async ({ page }) => {
+  // 1. Подготовка (Arrange)
+  // 2. Действие (Act)
+  // 3. Проверка (Assert)
+});
