3cfc32f172721d61eb73df3b3678853282415d04
Rombt <rombtnet@gmail.com>
2026-01-21 16:04:52 +0200

    structure of files

---
+++ src/browser/Browser.ts
@@ -0,0 +1,13 @@
+export interface Browser {
+  open(url: string): Promise<void>;
+
+  getHtml(): Promise<string>;
+
+  find(selector: string): Promise<boolean>;
+
+  getAttribute(selector: string, name: string): Promise<string | null>;
+
+  download(url: string, saveAs: string): Promise<void>;
+
+  close(): Promise<void>;
+}
