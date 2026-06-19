f1fe7ac9e211966b6ed47bde9e0e4d8a486e5673
Rombt <rombtnet@gmail.com>
2026-06-19 16:53:33 +0300

    PageImageSourceSrm.ts

---
+++ dist/source/sources/PageImageSourceSrm.js
@@ -0,0 +1,77 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CheckSearchResultsStep_1 = require("../steps/checks/CheckSearchResultsStep");
+const CheckPageSkuStep_1 = require("../steps/checks/CheckPageSkuStep");
+const SearchGalleryStep_1 = require("../steps/searchElements/SearchGalleryStep");
+const CollectImgStep_1 = require("../steps/collectElements/CollectImgStep");
+const CollectDescriptionStep_1 = require("../steps/collectElements/CollectDescriptionStep");
+exports.default = {
+    create(deps) {
+        return new PageImageSourceSrm(deps.flowRunner);
+    },
+};
+class PageImageSourceSrm {
+    flowRunner;
+    constructor(flowRunner) {
+        this.flowRunner = flowRunner;
+    }
+    supports(task) {
+        return task.metadata.target_website === 'https://srmknives.com.ua/search?search={{sku_prod}}';
+    }
+    async execute(ctx) {
+        ctx.stepParams = new Map([
+            [
+                CheckSearchResultsStep_1.default,
+                {
+                    strategy: 'DefaultSearchResultsStrategy',
+                    linkSelector: 'div.product-teaser__image--wrapper > a',
+                    emptySelector: 'div.view-empty',
+                    emptySelectorText: 'нічого не знайдено',
+                },
+            ],
+            [CheckPageSkuStep_1.default, { pageSkuSelector: 'div.field-product-vendor-code__item.field__item' }],
+            [SearchGalleryStep_1.default, { gallerySelector: '[data-once="product-full-slider"]' }],
+            [
+                CollectImgStep_1.default,
+                {
+                    strategy: 'DefaultCollectImagesStrategy',
+                    stopProcessing: false,
+                },
+            ],
+            [
+                CollectDescriptionStep_1.default,
+                {
+                    containers: ['div.field-product-desc__item.field__item'],
+                    removeSelectors: ['h2'],
+                    expand: false,
+                    separator: '\n',
+                },
+            ],
+            // ['*', { retry: 2 }], // глобальный fallback
+        ]);
+        const startStep = ctx.stepFactory.create('OpenSearchPageStep');
+        await this.flowRunner.run(startStep, ctx);
+        ctx.logger?.debug('Processing of the PageImageSourceSrm is finished', {
+            component: 'PageImageSourceSrm',
+            method: 'execute()',
+            action: 'await this.flowRunner.run(startStep, ctx)',
+            data: {
+                ctx: ctx,
+            },
+        });
+        const product = ctx.input.product;
+        const sku = product.sku;
+        const images = {};
+        if (ctx.state.images?.length) {
+            images[sku] = ctx.state.images;
+            images[sku].idProduct = product.id_product;
+        }
+        return {
+            data: {
+                images,
+                html: ctx.state.html,
+            },
+            errors: ctx.errors,
+        };
+    }
+}
